697a284437d229b8033e5034b7f92d723231e2a9
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-23 16:39:03 +0100

    comments

---
+++ ex_00_intro/comments.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+# Comments
+# They are lines that don't get compiled. Only important stuff and structural things need to be commented.
+
+# Single line
+
+# Inline comments
+name = 'greg' # TODO change to actual name.
+
+# Multiline
+"""
+In this section, 
+we will explore single line comments,
+inline comments, and multiline comments.
+"""
