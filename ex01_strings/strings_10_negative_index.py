436e3fda616ec1bc6720a9d611cd438a58ee58d4
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-27 17:27:53 +0200

    negative index

---
+++ ex01_strings/strings_10_negative_index.py
@@ -0,0 +1,14 @@
+# It is used to work starting from the back, to reverse the flow
+
+sentence = 'The quick brown fox jumped over the lazy dog'
+print(sentence[-3]) # it will be the 'd', because it doesn't index as '0' at the end.
+
+# We can also use it for ranges
+print(sentence[-3:]) # this will print 'until the third char from behind", dog.
+
+
+
+
+
+
+
