af32e8233caa7f3579aeb66f107a5d936961a02b
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-27 16:37:43 +0200

    interpolation

---
+++ ex01_strings/strings_6_string_interpolation.py
@@ -0,0 +1,14 @@
+# Dynamic strings, code inside the strings
+
+name = 'Kristine'
+greeting = f'Hi {name}' # F as in printf, format ans stuff. In python it's with '{}'
+print(greeting)
+
+# It works with any code you want
+sum = f'This is the sum 2+2: {2 + 2}'
+print(sum)
+
+# To actually show brackets you need to put double brackets
+brackets = 'Show the brackets: {{here they are}}'
+print(brackets)
+
