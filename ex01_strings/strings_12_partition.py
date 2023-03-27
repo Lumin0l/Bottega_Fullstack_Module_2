e8ed1093e9ac8cb85661fa7790b44c2c1fb5cae2
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-27 17:42:26 +0200

    partition

---
+++ ex01_strings/strings_12_partition.py
@@ -0,0 +1,20 @@
+# Introduction to collections
+# Strings can be connected and broken
+
+heading = "Python: An Introduction" # lets say I want to brake it in header and subheader
+
+header, _, subheader = heading.partition(': ') # it takes the element you give as partition, and returns a 'touple collection', different strings and will assign them to what whe marked.
+
+"""
+The convention to store elements you don't wanna use is the "_", that's why we stored the partition element there.
+"""
+
+print(header)
+print(subheader)
+print(_)
+
+
+
+
+
+
