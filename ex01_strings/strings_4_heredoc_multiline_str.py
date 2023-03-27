b787a80ab2bc1a5cbff65410600e82de80e2fd25
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-27 15:39:00 +0200

    heredoc 2

--- ex01_strings/strings_4_heredoc_multiline_str.py
+++ ex01_strings/strings_4_heredoc_multiline_str.py
@@ -22,13 +22,21 @@ Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
 """.strip() # More or less like "split in c"
 
 print(content)
-
+print('-- separator --')
 print(content_2)
- 
 
+# Same with 'repr'
 
+# 'repr()' stands for "printable representation" and shows the special characters like \n in it.
+print('-- separator --')
+print(repr(content))
+print('--separator--')
 
- 
+# If we copy the text with special chars, que can save it in a single line:
+
+content_3 = ' \nNullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.\n\nVestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.\n\nInteger posuere erat a ante venenatis dapibus posuere velit aliquet.\n'
+
+print(content_3) 
  
 
 
