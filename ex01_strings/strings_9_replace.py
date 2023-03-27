00cf723102546f30c160ac7a7228b903f81d08db
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-27 17:23:19 +0200

    replace

---
+++ ex01_strings/strings_9_replace.py
@@ -0,0 +1,14 @@
+# It's used to replace or reasign a string value
+
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+sentence = sentence.replace('quick', 'fat') # it is used to replace specific things inside a string. What it does is the same as "sentence = 'The fat brown...'"
+
+print(sentence)
+
+
+
+
+
+
+
