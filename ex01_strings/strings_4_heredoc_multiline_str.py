ac563cb6e14b4a878c7e6b066ca6d2e7cee04f3f
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-27 09:36:03 +0200

    heredoc

---
+++ ex01_strings/strings_4_heredoc_multiline_str.py
@@ -0,0 +1,34 @@
+# Heredoc or multi line strings
+
+# Couple ways, 1:
+# will count the first \n
+content = """ 
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+"""
+
+# 2: strip function.
+
+
+content_2 = """
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+""".strip() # More or less like "split in c"
+
+print(content)
+
+print(content_2)
+ 
+
+
+
+ 
+ 
+
+
