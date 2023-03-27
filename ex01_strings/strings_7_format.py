9a605f9368440357cbbbfae4e53f31b2d2b2b85d
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-27 17:04:32 +0200

    .format

---
+++ ex01_strings/strings_7_format.py
@@ -0,0 +1,23 @@
+# Apparently not too useful according to the guy, but yeah, formating
+name = 'Kristine'
+age = 12
+product = 'Python elearning course'
+
+greeting = "Hi {0}, you are listed as {1} years old and you have purchased: {2}".format(name, age, product)
+# So, we use format to create an array of elements as input and then listing their order we use them in the variable
+
+print(greeting)
+print('\nseparator\n')
+
+greeting_2 = "The product you've bought is {2}, so {0}, enjoy your coding for your birthday number {1}. I add anothe thing here just to see {3}".format(name, age, product, 'STRING')
+
+print(greeting_2)
+
+
+
+
+
+
+
+
+
