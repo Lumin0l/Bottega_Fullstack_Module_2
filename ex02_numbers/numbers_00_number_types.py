d3e7367c22eb5452fc56df5467b612c91fea2ff8
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-29 13:03:10 +0200

    number types

---
+++ ex02_numbers/numbers_00_number_types.py
@@ -0,0 +1,13 @@
+# Different data types
+
+product_id = 123 # int. You do'nt need to declare it, the interpreter will do that automatically.
+
+sale_price = 14.99 # float. There is a decimal library. Floats are not so precise and stable.
+
+tip_percentage = 1/5 # fraction. If run, python will reeturn 0.2 automatically.
+
+# Calculations are done dynamically
+new_product = 150
+print(sale_price + new_product) # this will return 164.99, mixing the int with the float and returning a float.
+
+
