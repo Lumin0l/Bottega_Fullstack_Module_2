16f37cf5b8a7a8387d861564487c0426af090ca2
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-29 13:25:24 +0200

    PEMDAS

---
+++ ex02_numbers/numbers_03_order_of_operations.py
@@ -0,0 +1,12 @@
+# How the order of operations work in python
+
+calculation = 8 + 2 * 5 - (9 + 2) ** 2 
+print(calculation) # like always: Parentheses, then berreketak, then multiplications and divisions, then additions and subtractions
+'''
+P -> Please: P for Parentheses ()
+E -> Excuse: E for Exponent **
+M -> My:     M for Multiplications *
+D -> Dear:   D for Division /
+A -> Aunt:   A for Addition +
+S -> Sally:  S for Subtraction -
+'''
