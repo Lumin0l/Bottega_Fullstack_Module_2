a5d3f18a00fe30058e57fb7b6c90705b2e2ce440
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-29 13:15:08 +0200

    operators

---
+++ ex02_numbers/numbers_01_math_operators.py
@@ -0,0 +1,29 @@
+# Operators and how they work in python
+# They will be inside print statements for visualization, but they work the same in functions or variables.
+
+print('addition')
+print(100 + 42)
+# or
+addition = 100 + 42
+print(addition)
+
+print('subtraction')
+print(100 - 42.3) # different types can be used, int - float
+
+print(division)
+print(100 / 42)
+
+print('multiplication')
+print(100 * 42)
+
+print('modulus')
+print(100 % 42) # to get how many remainder lines there are. 
+
+print('exponents') # basque berreketak
+print(11 ** 2)
+
+# Now new stuff
+
+print('floor division') # it eliminates the things right to the comma, kind of like modulo, but it opposite
+print(100 // 42)
+
