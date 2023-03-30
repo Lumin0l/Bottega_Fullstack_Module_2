26e53022ecd1e89f51b26f86caa58f7d208f18f2
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-30 16:53:58 +0200

    adding stuff to dictionaries

---
+++ ex03_collections/collections_16_dictionaries_add_key-value-pairs.py
@@ -0,0 +1,11 @@
+# How to add stuff to dictionaries:
+teams = {
+  "astros": ["Altuve", "Correa", "Bregman"],
+  "angels": ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+}
+
+# Just type it bro
+teams['red sox'] = ['Price', 'Betts'] # as seen, as long as they are passed as strings, keys can be as long as it's needed.
+
+print(teams)
