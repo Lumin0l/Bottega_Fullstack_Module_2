80f6a03b9373242f3ed29a73cc242bf22eb571c7
Lumin0l <idelaiglesia.m@gmail.com>
2023-03-31 13:31:42 +0200

    Tuple slices

---
+++ ex03_collections/collections_25_Tuples_slices.py
@@ -0,0 +1,14 @@
+# You can slice tuples same as strings
+# The syntax is nearly identical.
+
+post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
+
+print(post[:2]) # 'Python Basics' and 'Intro guide to...'
+
+# All the same rules apply with tuples, so 3 elements: start, stop and interval:
+print(post[1::2]) # Print from the first element on, to the end, every 2 elements.
+
+
+
+
+
